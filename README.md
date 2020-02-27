59dff409a7e363e7c2e97ececb44852b0a32e7ad
vauxhall <zero.griffin@gmail.com>
2020-02-27 10:21:56 +0100

    Add project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deforestation
+
+Deforestation Apocalypse
---
+++ macOS/App.swift
@@ -0,0 +1,18 @@
+import AppKit
+
+@NSApplicationMain final class App: NSApplication, NSApplicationDelegate {
+    required init?(coder: NSCoder) { nil }
+    override init() {
+        super.init()
+        delegate = self
+        UserDefaults.standard.set(false, forKey: "NSFullScreenMenuItemEverywhere")
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_: NSApplication) -> Bool {
+        true
+    }
+    
+    func applicationWillFinishLaunching(_: Notification) {
+        NSWindow().makeKeyAndOrderFront(nil)
+    }
+}
