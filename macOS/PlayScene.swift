5dc9423ec00d938809aa23f139a2c167742399a5
vauxhall <zero.griffin@gmail.com>
2020-02-27 15:07:59 +0100

    Add camera

--- macOS/PlayScene.swift
+++ macOS/PlayScene.swift
@@ -1,5 +1,17 @@
-import SpriteKit
+import GameplayKit
 
 final class PlayScene: SKScene {
+    private(set) weak var area: SKTileMapNode!
+    private(set) var grid: GKGridGraph<GKGridGraphNode>!
     
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        grid = .init(fromGridStartingAt: .zero, width: 258, height: 160, diagonalsAllowed: false)
+        area = childNode(withName: "Area") as? SKTileMapNode
+        
+        let camera = SKCameraNode()
+        camera.position = .init(x: 3500, y: 2500)
+        addChild(camera)
+        self.camera = camera
+    }
 }
--- macOS/PlayState.swift
+++ macOS/PlayState.swift
@@ -1,10 +1,23 @@
 import GameplayKit
 
 final class PlayState: State {
+    private weak var scene: PlayScene!
+    
     override func didEnter(from: GKState?) {
         super.didEnter(from: from)
         if !(view.scene is PlayScene) {
-            view.presentScene(GKScene(fileNamed: "PlayScene")!.rootNode as! PlayScene, transition: .fade(withDuration: 1.5))
+            let scene = GKScene(fileNamed: "PlayScene")!.rootNode as! PlayScene
+            view.presentScene(scene, transition: .fade(withDuration: 1.5))
+            self.scene = scene
         }
+    }
+    
+    override func control() {
+        guard let press = self.press.popLast() else { return }
+//        scene.area.tilede
+//        
+//        let group = SKTileGroup(tileDefinition: .init(texture: .init(imageNamed: "tile_desert")))
+//        area.tileSet.tileGroups.append(group)
+//        area.setTileGroup(group, forColumn: 258 / 2, row: 80)
     }
 }
