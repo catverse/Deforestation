e2dec4da95f236d426c00e7e51cf39865ee909af
vauxhall <zero.griffin@gmail.com>
2020-02-27 13:53:56 +0100

    Add world map

---
+++ macOS/PlayScene.swift
@@ -0,0 +1,5 @@
+import SpriteKit
+
+final class PlayScene: SKScene {
+    
+}
--- macOS/PlayState.swift
+++ macOS/PlayState.swift
@@ -1,5 +1,10 @@
 import GameplayKit
 
 final class PlayState: State {
-    
+    override func didEnter(from: GKState?) {
+        super.didEnter(from: from)
+        if !(view.scene is PlayScene) {
+            view.presentScene(GKScene(fileNamed: "PlayScene")!.rootNode as! PlayScene, transition: .fade(withDuration: 1.5))
+        }
+    }
 }
