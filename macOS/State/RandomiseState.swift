ef6aa4c8e8ba01710c16a181f6e199cba842b968
vauxhall <zero.griffin@gmail.com>
2020-03-26 15:37:53 +0100

    Show loading dialog

--- macOS/State/RandomiseState.swift
+++ macOS/State/RandomiseState.swift
@@ -2,6 +2,7 @@ import GameplayKit
 
 final class RandomiseState: State {
     private weak var scene: PlayScene!
+    private weak var base: SKShapeNode!
     private var x = 0
     
     override func didEnter(from: GKState?) {
@@ -11,18 +12,24 @@ final class RandomiseState: State {
         self.scene = scene
         view.presentScene(scene, transition: .fade(withDuration: 1.5))
         
-        let base = SKShapeNode(rect: .init(x: -100, y: -50, width: 200, height: 100), cornerRadius: 4)
+        let base = SKShapeNode(rect: .init(x: -70, y: -20, width: 140, height: 40), cornerRadius: 6)
+        base.fillColor = .init(white: 0, alpha: 0.9)
+        base.strokeColor = .clear
+        base.alpha = 0
+        self.base = base
+        
         let loading = SKLabelNode(attributedText: .init(string: .key("RandomiseState.loading"),
                                                         attributes: [.font: NSFont.systemFont(ofSize: 14, weight: .bold),
                                                                      .foregroundColor: NSColor.white]))
-        loading.alpha = 0
         loading.verticalAlignmentMode = .center
-        scene.camera!.addChild(loading)
-        loading.run(.repeatForever(.sequence([.fadeIn(withDuration: 0.5), .wait(forDuration: 1), .fadeOut(withDuration: 0.5)])))
+        base.addChild(loading)
+        scene.camera!.addChild(base)
+        base.run(.fadeIn(withDuration: 0.5))
     }
     
     override func update(deltaTime: TimeInterval) {
         guard x < scene.grid.gridWidth else {
+            base.removeFromParent()
             stateMachine!.enter(PlayState.self)
             return
         }
